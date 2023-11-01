e83007719fb444a4a9550465fadc483e72e8b765
geovanagv <geovana.veloso@iesb.edu.br>
2023-10-31 21:38:59 -0300

    atividade crud

---
+++ App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { PaperProvider } from 'react-native-paper';
+import DrawerRoutes from './src/routes/DrawerRoutes';
+import Toast from 'react-native-toast-message';
+
+export default function App() { 
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <DrawerRoutes />
+      </NavigationContainer>
+      <Toast /> 
+    </PaperProvider>
+  );
+} 
